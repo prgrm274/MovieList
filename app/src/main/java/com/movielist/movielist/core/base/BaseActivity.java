98268f93fa74bb046a5b03f5d5952840530d575f
mobilermhku <riki@primesystem.id>
2018-12-31 13:41:39 +0700

    90 added 1 modified

---
+++ app/src/main/java/com/movielist/movielist/core/base/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.movielist.movielist.core.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * //Jum 28-12-18 185533
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
